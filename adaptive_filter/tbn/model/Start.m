693705f75bf6c5efc38669436c6ba7b4a4ee9e0b
loris19905 <129176549+loris19905@users.noreply.github.com>
2023-09-21 15:42:59 +0300

    Скрипты моделей
    Добавление переменных для управления скриптами

--- adaptive_filter/tbn/model/Start.m
+++ adaptive_filter/tbn/model/Start.m
@@ -1,3 +1,21 @@
+%% Параметры управления скриптом
+
+INPUT_FREQ_VECTOR = 0.2;
+BANDWIDTH         = 0.35;
+FREQ_RANGE        = 0.01:0.0001:BANDWIDTH;
+
+WORDLENGTH        = 14;
+FRACTIONAL_LENGTH = 6;
+    
+FILTER_MODE       = 'integrator'; % 'integrator', 'differentiator', 'both'
+MAKE_PLOT         = 0;
+
+FS   = 1;
+t    = 0:1/FS:5/min(INPUT_FREQ_VECTOR);
+Time = length(t)/FS;
+
+%% Тест интегратора
+
 %Предварительно надо запустить программу с определением разрядности
 %коэффициентов, закомментировать первую часть модели в Simulink  и
 %раскомментировать вторую.
@@ -7,99 +25,97 @@
 %интегратора
 %значения амплитуд для одной частоты записываю в столбец
 
-f                 = [0.01, 0.02, 0.04, 0.08, 0.17, 0.35];
-wordlength        = 14;
-fractional_length = 6;
-
-max_amp = 2^(wordlength - fractional_length - 1) - 1 + 0.5*(1 - 0.5^(fractional_length))/(1 - 0.5);
-lsb = 2^(-fractional_length);
-
-amp = [];
-for i = 1:length(f)
-    divider = 2;
-    for j = 1:10
-        if j == 1
-            amp(j,i) = max_amp/10^(ceil(dB_integrator_fp(find(freq == f(i))))/20)/2;
-            if amp(j,i) > max_amp/2
-                amp(j,i) = max_amp/2;
+if strcmp(FILTER_MODE, 'integrator') || strcmp(FILTER_MODE, 'both')
+    
+    max_amp = 2^(WORDLENGTH - FRACTIONAL_LENGTH - 1) - 1 + 0.5*(1 - 0.5^(FRACTIONAL_LENGTH))/(1 - 0.5);
+    lsb = 2^(-FRACTIONAL_LENGTH);
+    
+    amp = [];
+    for i = 1:length(INPUT_FREQ_VECTOR)
+        divider = 2;
+        for j = 1:10
+            if j == 1
+                amp(j,i) = max_amp/10^(ceil(dB_integrator_fp(find(freq == INPUT_FREQ_VECTOR(i))))/20)/2;
+                if amp(j,i) > max_amp/2
+                    amp(j,i) = max_amp/2;
+                end
+                
+                while amp(j,i)/(divider^9) < lsb
+                    divider = divider * 0.9;
+                end
+            else
+                amp(j,i) = amp(j-1,i)/divider;
             end
-            
-            while amp(j,i)/(divider^9) < lsb
-                divider = divider * 0.9;
-            end
-        else
-            amp(j,i) = amp(j-1,i)/divider;
         end
     end
+
+    ctrl = ones([length(t), 1]);
+    ctrl = [t', ctrl];
+    max_error_integr = [];
+    for i = 1:length(INPUT_FREQ_VECTOR)
+        test_signal = sin(2*pi*INPUT_FREQ_VECTOR(i)*t')*(amp(:,i))';
+        test_signal = [t', test_signal];
+        sim("models_diff_integr.slx");
+        error_data = ans.error;
+        max_error_integr(end+1) = max(max(error_data));
+    end
+
 end
-%%
-%тест в режиме интегратора
-ts = 1;
-t = 0:ts:5/min(f);
-Time = length(t)*ts;
-ctrl = ones([length(t), 1]);
-ctrl = [t', ctrl];
-max_error_integr = [];
-for i = 1:length(f)
-    test_signal = sin(2*pi*f(i)*t')*(amp(:,i))';
-    test_signal = [t', test_signal];
-    sim("models_diff_integr.slx");
-    error_data = ans.error;
-    max_error_integr(end+1) = max(max(error_data));
-end
-%%
+
+%% Тест дифференциатора
 %определим диапазон амплитуд тестовых сигналов для дифференциатора
 %значения амплитуд для одной частоты записываю в столбец
-
-amp = [];
-for i = 1:length(f)
-    divider = 2;
-    for j = 1:10
-        if j == 1
-            amp(j,i) = max_amp/10^(dB_diff_fp(find(freq == f(i)))/20);
-            if amp(j,i) > max_amp
-                amp(j,i) = max_amp;
+if strcmp(FILTER_MODE, 'differentiator') || strcmp(FILTER_MODE, 'both')
+    amp = [];
+    for i = 1:length(INPUT_FREQ_VECTOR)
+        divider = 2;
+        for j = 1:10
+            if j == 1
+                amp(j,i) = max_amp/10^(dB_diff_fp(find(freq == INPUT_FREQ_VECTOR(i)))/20);
+                if amp(j,i) > max_amp
+                    amp(j,i) = max_amp;
+                end
+                while amp(j,i)/(divider^9) < lsb
+                    divider = divider*0.9;
+                end
+            else
+                amp(j,i) = amp(j-1,i)/divider;
             end
-            while amp(j,i)/(divider^9) < lsb
-                divider = divider*0.9;
-            end
-        else
-            amp(j,i) = amp(j-1,i)/divider;
         end
     end
+
+    ctrl = zeros([length(t), 1]);
+    ctrl = [t', ctrl];
+    max_error_diff = [];
+    for i = 1:length(INPUT_FREQ_VECTOR)
+        test_signal = sin(2*pi*f(i)*t')*(amp(:,i))';
+        test_signal = [t', test_signal];
+        sim("models_diff_integr.slx");
+        error_data = ans.error;
+        max_error_diff(end+1) = max(max(error_data));    
+    end
 end
-%%
-%тест в режиме дифференциатора
-ts = 1;
-t = 0:ts:5/min(f);
-Time = length(t)*ts;
-ctrl = zeros([length(t), 1]);
-ctrl = [t', ctrl];
-max_error_diff = [];
-for i = 1:length(f)
-    test_signal = sin(2*pi*f(i)*t')*(amp(:,i))';
-    test_signal = [t', test_signal];
-    sim("models_diff_integr.slx");
-    error_data = ans.error;
-    max_error_diff(end+1) = max(max(error_data));    
+
+%% Вывод вектора ошибок
+
+if (MAKE_PLOT)
+    disp("Integr error:")
+    max_error_integr
+    disp("Diff error:")
+    max_error_diff
+    graph = plot(INPUT_FREQ_VECTOR, max_error_integr/lsb, "DisplayName", "Максимальная ошибка в режиме интегратора");
+    graph.LineWidth = 1.5;
+    hold on
+    graph = plot(INPUT_FREQ_VECTOR, max_error_diff/lsb, "DisplayName", "Максимальная ошибка в режиме дифференциатора");
+    graph.LineWidth = 1.5;
+    xlabel("Нормированная частота")
+    ylabel("Ошибка, МЗР")
+    ax = gca;
+    set(get(ax,'YLabel'),'Rotation',90);
+    ax.FontName = "Times New Roman";
+    ax.FontSize = 10;
+    grid on
+    legend("boxoff")
+    ylim([0, 4])
+    hold off
 end
-%%
-disp("Integr error:")
-max_error_integr
-disp("Diff error:")
-max_error_diff
-graph = plot(f, max_error_integr/lsb, "DisplayName", "Максимальная ошибка в режиме интегратора");
-graph.LineWidth = 1.5;
-hold on
-graph = plot(f, max_error_diff/lsb, "DisplayName", "Максимальная ошибка в режиме дифференциатора");
-graph.LineWidth = 1.5;
-xlabel("Нормированная частота")
-ylabel("Ошибка, МЗР")
-ax = gca;
-set(get(ax,'YLabel'),'Rotation',90);
-ax.FontName = "Times New Roman";
-ax.FontSize = 10;
-grid on
-legend("boxoff")
-ylim([0, 4])
-hold off
--- adaptive_filter/tbn/model/estimate_resolution.m
+++ adaptive_filter/tbn/model/estimate_resolution.m
@@ -1,4 +1,7 @@
-%Начальные данные
+%% Параметры запуска
+MAKE_PLOT = 0;
+
+%% Начальные данные
 
 Num = [-0.016686934099368,
        0.195507800433580,
@@ -17,8 +20,7 @@ resolution_integr = ones([1,3]);
 %limit - ограничение ошибки по заданию, размерность - дБ
 
 limit = 0.1;
-%%
-%Поиск разрядности для числа 0,358
+%% Поиск разрядности для числа 0,358
 % Считаем, что b_Tick(1) и b_Tick(3) одно и то же число, так как в таком
 % случае ФЧХ линейна
 %b_Tick(2) оставляем double
@@ -35,59 +37,71 @@ for i = 1:max_resolution
     dB_invest_func = 20 * log10(abs([double(fi(b_Tick(1), 0, i + 1, i)), b_Tick(2), double(fi(b_Tick(3),0,i + 1,i))] * mult ./ (a_Tick * mult)));
     max_error(end+1) = max(abs(dB_invest_func - dB_func_Tick));   
 end
-graph = plot(1:max_resolution, max_error);
-graph.LineWidth = 1.5;
-xlabel("Разрядность, бит")
-ylabel("Максимальная ошибка, дБ")
-ax = gca;
-set(get(ax,'YLabel'),'Rotation',90);
-ax.FontName = "Times New Roman";
-ax.FontSize = 10;
-grid on
-%%
-%Выбираем разрядность коэффициентов 1 и 3
+
+if (MAKE_PLOT)
+    graph = plot(1:max_resolution, max_error);
+    graph.LineWidth = 1.5;
+    xlabel("Разрядность, бит")
+    ylabel("Максимальная ошибка, дБ")
+    ax = gca;
+    set(get(ax,'YLabel'),'Rotation',90);
+    ax.FontName = "Times New Roman";
+    ax.FontSize = 10;
+    grid on
+end
+
+%% Выбираем разрядность коэффициентов 1 и 3
 
 pos_res              = find(max_error < limit);
 resolution_integr(1) = pos_res(1);
 resolution_integr(3) = pos_res(1);
-%%
-%Ищем второй коэффициент, фиксируем разрядность первого и второго
+
+%% Ищем второй коэффициент, фиксируем разрядность первого и второго
+
 max_error = [];
 for i = 1:max_resolution
     dB_invest_func = 20 * log10(abs([double(fi(b_Tick(1), 0, resolution_integr(1) + 1, resolution_integr(1))), double(fi(b_Tick(2), 0, i + 1, i)), double(fi(b_Tick(3),0,resolution_integr(3) + 1,resolution_integr(3)))]*mult./(a_Tick*mult)));
     max_error(end+1) = max(abs(dB_invest_func - dB_func_Tick));  
 end
-graph = plot(1:max_resolution, max_error);
-graph.LineWidth = 1.5;
-xlabel("Разрядность, бит")
-ylabel("Максимальная ошибка, дБ")
-ax = gca;
-set(get(ax,'YLabel'),'Rotation',90);
-ax.FontName = "Times New Roman";
-ax.FontSize = 10;
-grid on
-%%
-%Выбираем разрядность коэффициента 2
+
+if (MAKE_PLOT)
+    graph = plot(1:max_resolution, max_error);
+    graph.LineWidth = 1.5;
+    xlabel("Разрядность, бит")
+    ylabel("Максимальная ошибка, дБ")
+    ax = gca;
+    set(get(ax,'YLabel'),'Rotation',90);
+    ax.FontName = "Times New Roman";
+    ax.FontSize = 10;
+    grid on
+end
+
+%% Выбираем разрядность коэффициента 2
+
 pos_res              = find(max_error < limit);
 resolution_integr(2) = pos_res(1);
-%%
-%Отображение ошибки модели интегратора
+
+%% Отображение ошибки модели интегратора
 coeff_integrator = b_Tick;
 for i = 1:length(b_Tick)
     coeff_integrator(i) = double(fi(b_Tick(i), 1, resolution_integr(i) + 2, resolution_integr(i)));
 end
 dB_integrator_fp = 20 * log10(abs(coeff_integrator * mult./(a_Tick * mult)));
 error_integrator = abs(dB_integrator_fp - dB_func_Tick);
-graph = plot(freq, error_integrator);
-graph.LineWidth = 1.5;
-xlabel("Нормированная частота")
-ylabel("Ошибка модели с фиксированной точкой, дБ")
-ax = gca;
-set(get(ax,'YLabel'),'Rotation',90);
-ax.FontName = "Times New Roman";
-ax.FontSize = 10;
-grid on
-%%
+
+if (MAKE_PLOT)
+    graph = plot(freq, error_integrator);
+    graph.LineWidth = 1.5;
+    xlabel("Нормированная частота")
+    ylabel("Ошибка модели с фиксированной точкой, дБ")
+    ax = gca;
+    set(get(ax,'YLabel'),'Rotation',90);
+    ax.FontName = "Times New Roman";
+    ax.FontSize = 10;
+    grid on
+end
+
+%% Коэффициенты дифференциатора
 %Далее исследуем коэффициенты дифференциатора
 %Методы все те же самые
 order              = 0:9;
@@ -100,60 +114,71 @@ max_resolution     = 20;
 coeff              = Num;
 for j = 1:(order(end)+1)/2
     max_error = [];
+    
     if j > 1
         coeff(j-1)     = double(fi(coeff(j-1), 1, resolution_diff(j-1) + 2, resolution_diff(j-1)));
         coeff(end-j+2) = double(fi(coeff(end-j+2), 1, resolution_diff(j-1) + 2, resolution_diff(j-1)));
     end
+
     for i = 1:max_resolution
         coeff(j)         = double(fi(Num(j), 1, i+2, i));
         coeff(end-j+1)   = double(fi(Num(end-j+1), 1, i+2, i));
         dB_invest_func   = 20*log10(abs(coeff*mult));
         max_error(end+1) = max(abs(dB_invest_func - dB_func_diff));
     end
-    figure(j)
-    graph = plot(1:max_resolution, max_error);
+
+    if (MAKE_PLOT)
+        figure(j)
+        graph = plot(1:max_resolution, max_error);
+        graph.LineWidth = 1.5;
+        xlabel("Разрядность, бит")
+        ylabel("Максимальная ошибка, дБ")
+        ax = gca;
+        set(get(ax,'YLabel'),'Rotation',90);
+        ax.FontName = "Times New Roman";
+        ax.FontSize = 10;
+        grid on
+        pos_res                  = find(max_error < limit);
+        resolution_diff(j)       = pos_res(1);
+        resolution_diff(end-j+1) = pos_res(1);
+    end
+
+end
+%% Покажем распределение ошибки вдоль полосы для дифференциатора
+
+dB_diff_fp = 20 * log10(abs(coeff * mult));
+error_diff = abs(dB_diff_fp - dB_func_diff);
+
+if (MAKE_PLOT)
+    graph = plot(freq, error_diff);
     graph.LineWidth = 1.5;
-    xlabel("Разрядность, бит")
-    ylabel("Максимальная ошибка, дБ")
+    xlabel("Нормированная частота")
+    ylabel("Ошибка модели с фиксированной точкой, дБ")
     ax = gca;
     set(get(ax,'YLabel'),'Rotation',90);
     ax.FontName = "Times New Roman";
     ax.FontSize = 10;
     grid on
-    pos_res                  = find(max_error < limit);
-    resolution_diff(j)       = pos_res(1);
-    resolution_diff(end-j+1) = pos_res(1);
 end
-%%
-%Покажем распределение ошибки вдоль полосы для дифференциатора
-dB_diff_fp = 20 * log10(abs(coeff * mult));
-error_diff = abs(dB_diff_fp - dB_func_diff);
 
-graph = plot(freq, error_diff);
-graph.LineWidth = 1.5;
-xlabel("Нормированная частота")
-ylabel("Ошибка модели с фиксированной точкой, дБ")
-ax = gca;
-set(get(ax,'YLabel'),'Rotation',90);
-ax.FontName = "Times New Roman";
-ax.FontSize = 10;
-grid on
-%%
-%график совместный с указанием линии критерия
-graph = plot(freq, error_diff, 'DisplayName', 'Ошибка для модели дифференциатора');
-graph.LineWidth = 1.5;
-hold on
-graph = plot(freq, error_integrator, 'DisplayName', 'Ошибка для модели интегратора');
-graph.LineWidth = 1.5;
-graph = plot(linspace(0,0.35, 2), limit*ones([1,2]), 'r--', 'DisplayName', 'Предельная ошибки');
-graph.LineWidth = 1.5;
-xlabel("Нормированная частота")
-ylabel("Ошибка модели с фиксированной точкой, дБ")
-ax = gca;
-set(get(ax,'YLabel'),'Rotation',90);
-ax.FontName = "Times New Roman";
-ax.FontSize = 10;
-grid on
-legend('boxoff')
-ylim([0, 1.2*limit])
-hold off
+%% График совместный с указанием линии критерия
+
+if (MAKE_PLOT)
+    graph = plot(freq, error_diff, 'DisplayName', 'Ошибка для модели дифференциатора');
+    graph.LineWidth = 1.5;
+    hold on
+    graph = plot(freq, error_integrator, 'DisplayName', 'Ошибка для модели интегратора');
+    graph.LineWidth = 1.5;
+    graph = plot(linspace(0,0.35, 2), limit*ones([1,2]), 'r--', 'DisplayName', 'Предельная ошибки');
+    graph.LineWidth = 1.5;
+    xlabel("Нормированная частота")
+    ylabel("Ошибка модели с фиксированной точкой, дБ")
+    ax = gca;
+    set(get(ax,'YLabel'),'Rotation',90);
+    ax.FontName = "Times New Roman";
+    ax.FontSize = 10;
+    grid on
+    legend('boxoff')
+    ylim([0, 1.2*limit])
+    hold off
+end
