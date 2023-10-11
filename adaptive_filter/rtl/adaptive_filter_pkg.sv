b52dc10f723b97894f902372fd64f348f4187fa0
loris19905 <129176549+loris19905@users.noreply.github.com>
2023-10-11 22:11:48 +0300

    Revert "hotfix"
    This reverts commit 582908c4a182b95354441db899431e8e3b1fe751.

--- adaptive_filter/rtl/adaptive_filter_pkg.sv
+++ adaptive_filter/rtl/adaptive_filter_pkg.sv
@@ -1,18 +1,17 @@
 package adaptive_filter_pkg;
 
 	localparam WORDLENGTH 		 = 14;
-	localparam INT32_SIZE 		 = 32;
 	localparam FRACTIONAL_LENGTH = 6;
 	
 	localparam FIR_DIFF_ORDER 	  = 9;
 	localparam FIR_DIFF_COEFF_NUM = (FIR_DIFF_ORDER + 1) >> 1;
 
-	localparam [FIR_DIFF_COEFF_NUM-1:0][INT32_SIZE-1:0] DIFF_COEFF_WL = {7, 6, 9, 9, 8};
-	localparam [FIR_DIFF_COEFF_NUM-1:0][INT32_SIZE-1:0] DIFF_COEFF_FL = {5, 4, 7, 7, 6};
+	localparam int DIFF_COEFF_WL [FIR_DIFF_COEFF_NUM-1:0] = {7, 6, 9, 9, 8};
+	localparam int DIFF_COEFF_FL [FIR_DIFF_COEFF_NUM-1:0] = {5, 4, 7, 7, 6};
 
-	localparam INTEGR_COEFF_NUM    						  			  = 2;
-	localparam [INTEGR_COEFF_NUM-1:0][INT32_SIZE-1:0] INTEGR_COEFF_WL = {6, 7};
-	localparam [INTEGR_COEFF_NUM-1:0][INT32_SIZE-1:0] INTEGR_COEFF_FL = {5, 6}; 
+	localparam INTEGR_COEFF_NUM    						  = 2;
+	localparam int INTEGR_COEFF_WL [INTEGR_COEFF_NUM-1:0] = {6, 7};
+	localparam int INTEGR_COEFF_FL [INTEGR_COEFF_NUM-1:0] = {5, 6}; 
 
 	logic [DIFF_COEFF_WL[0]-DIFF_COEFF_FL[0]-1:-DIFF_COEFF_FL[0]] fir_diff_coeff_a0 = 8'hFF;
 	logic [DIFF_COEFF_WL[1]-DIFF_COEFF_FL[1]-1:-DIFF_COEFF_FL[1]] fir_diff_coeff_a1 = 9'h019;
@@ -23,8 +22,8 @@ package adaptive_filter_pkg;
 	logic [INTEGR_COEFF_WL[0]-INTEGR_COEFF_FL[0]-1:-INTEGR_COEFF_FL[0]] fir_integr_coeff_a0 = 7'h17;
 	logic [INTEGR_COEFF_WL[1]-INTEGR_COEFF_FL[1]-1:-INTEGR_COEFF_FL[1]] fir_integr_coeff_a1 = 6'h29;
 	
-	localparam [FIR_DIFF_COEFF_NUM-1:0][INT32_SIZE-1:0] MULTYPLYERS_WL = {14, 14, 20, 19, 18};
-	localparam [FIR_DIFF_COEFF_NUM-1:0][INT32_SIZE-1:0] MULTYPLYERS_FL = {6 , 6 , 12, 11, 12};
+	localparam int MULTYPLYERS_WL [FIR_DIFF_COEFF_NUM-1:0] = {14, 14, 20, 19, 18};
+	localparam int MULTYPLYERS_FL [FIR_DIFF_COEFF_NUM-1:0] = {6 , 6 , 12, 11, 12};
 
 	localparam OP_SUMM_WL = 20;
 	localparam OP_SUMM_FL = 12;
