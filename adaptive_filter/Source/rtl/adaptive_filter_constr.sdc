7d5dddaa06d516c471aca775ac47f39a29a62a9d
loris19905 <129176549+loris19905@users.noreply.github.com>
2024-01-09 14:57:14 +0300

    Правки
    добавлены комментарии

--- adaptive_filter/Source/rtl/adaptive_filter_constr.sdc
+++ adaptive_filter/Source/rtl/adaptive_filter_constr.sdc
@@ -8,13 +8,18 @@
 ###FIle description: SDC-file for Synthesis
 ###Work Directory: /adaptive_filter/Source/rtl/ 
 
+### Задание единиц измерения
 set_units -time ns -capacitance pF
 
+### Объявление тактового сигнала
 create_clock -name clk -period 25 [get_ports clk]
 
+### Задание задержки по входу и выходу фильтра
 set_input_delay  	-clock clk -max 0.5 [get_ports  -filter {@port_direction == in}]
 set_output_delay 	-clock clk -max 0.5 [get_ports  -filter {@port_direction == out}]
 
+### Задание нестабильности тактового сигнала
 set_clock_uncertainty   -to clk 0.25
 
+### Задание выходной нагрузочной емкости
 set_load 		0.5 [all_outputs]
--- adaptive_filter/Source/rtl/adaptive_filter_pkg.sv
+++ adaptive_filter/Source/rtl/adaptive_filter_pkg.sv
@@ -4,25 +4,32 @@
 
 package adaptive_filter_pkg;
 
+	//Параметры, задающие ширину входных/выходных данных
 	localparam WORDLENGTH 		 = 14;
 	localparam FRACTIONAL_LENGTH = 6;
 	
+	//Вспомогательные параметры для построения КИХ-фильтра дифференциатора
 	localparam FIR_DIFF_ORDER 	  = 9;
 	localparam FIR_DIFF_COEFF_NUM = (FIR_DIFF_ORDER + 1) >> 1;
 
+	//Параметры ширины данных коэффициентов импульсной характеристики дифференциатора
 	parameter  int DIFF_COEFF_WL [FIR_DIFF_COEFF_NUM-1:0] = '{7, 6, 9, 9, 8};
 	parameter  int DIFF_COEFF_FL [FIR_DIFF_COEFF_NUM-1:0] = '{5, 4, 7, 7, 6};
 
+	//Вспомогательные параметры для интегратора + параметры ширины данных импульсной характеристики
 	localparam INTEGR_COEFF_NUM    						  = 2;
 	parameter  int INTEGR_COEFF_WL [INTEGR_COEFF_NUM-1:0] = '{6, 7};
 	parameter  int INTEGR_COEFF_FL [INTEGR_COEFF_NUM-1:0] = '{5, 6}; 
 	
+	//Параметры для описания ширины выходных данных умножителей
 	parameter int MULTYPLYERS_WL [FIR_DIFF_COEFF_NUM-1:0] = '{14, 14, 20, 19, 18};
 	parameter int MULTYPLYERS_FL [FIR_DIFF_COEFF_NUM-1:0] = '{6 , 6 , 12, 11, 12};
 
+	//Параметры для задания ширины данных сумматоров
 	localparam OP_SUMM_WL = 20;
 	localparam OP_SUMM_FL = 12;
 
+	//Параметры для задания ширины данных блоков вычитания
 	localparam OP_DIFF_WL = 15;
 	localparam OP_DIFF_FL = 6;
 
